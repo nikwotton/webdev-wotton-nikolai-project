988e61084a404ba0fbc52917bd4936f70067150f
Nikolai Wotton <wottonn@gmail.com>
2017-12-11 01:15:57 -0500

    made users searchable for admin

--- src/app/components/user/user-list/user-list.component.ts
+++ src/app/components/user/user-list/user-list.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {UserService} from '../../../services/user.service.client';
 import {ImageService} from '../../../services/image.service.client';
+import {NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
@@ -9,15 +11,34 @@ import {ImageService} from '../../../services/image.service.client';
 })
 export class UserListComponent implements OnInit {
 
+  @ViewChild('f') form: NgForm;
+
   userId: string;
   users: any;
+  searchMessage = '';
 
-  constructor(private userService: UserService, private imageService: ImageService) {
+  constructor(private userService: UserService, private imageService: ImageService,
+              private router: Router) {
   }
 
   ngOnInit() {
     this.userService.findAllUsers().subscribe((users: any) => {
       this.users = users;
+    });
+  }
+
+  updateSearch() {
+    if (this.form.value.search === '') {
+      this.searchMessage = '';
+      return;
+    }
+    this.userService.findUserByUsername(this.form.value.search).subscribe((data: any) => {
+      if (data === null) {
+        this.searchMessage = 'Username not found';
+        return;
+      }
+      this.searchMessage = '';
+      return this.router.navigate(['/users/' + data._id]);
     });
   }
 }
